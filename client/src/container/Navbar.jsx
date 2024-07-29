36850ab10716b0854361bcfeb43e244cfe8e842e
Nimish-Dureja-10 <durejanimish14@gmail.com>
2024-07-30 00:02:24 +0530

    Added top navbar and login page

--- client/src/container/Navbar.jsx
+++ client/src/container/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { BiLogoGmail } from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='h-16 bg-black border-b border-gray-800 flex justify-center items-center'>
+      <BiLogoGmail className='bg-white w-12 h-7' /> <h1 className='text-xl font-light text-white mx-2 font-mono'>REACHINBOX</h1>
+    </div>
   )
 }
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import googleLogo from "../assets/google.png"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='bg-black h-screen flex justify-center items-center pb-20'>
+      <div className='py-6 px-10 bg-neutral-900 border border-gray-500 rounded-2xl text-center flex gap-4 flex-col justify-center items-center'>
+        <h3 className='text-lg text-white my-2'>
+          Create a new account
+        </h3>
+        <button className='text-gray-300 border py-3 px-28 rounded-md mb-6 flex items-center gap-2 font-extralight'>
+          <img src={googleLogo} alt='google-logo' className='' />
+          Sign Up with Google
+        </button>
+        <button className='text-white rounded bg-blue-700 py-3 px-8 mb-4'>
+          Create an Account
+        </button>
+        <p className='text-gray-500 font-light mb-4'>Already have an account? <span className='text-gray-400 hover:underline cursor-pointer'>Sign In</span></p>
+      </div>
+    </div>
   )
 }
 
