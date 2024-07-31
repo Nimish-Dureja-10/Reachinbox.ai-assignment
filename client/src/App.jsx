9a32fc855113dd155ed134e3e158db01d22409c0
Nimish-Dureja-10 <durejanimish14@gmail.com>
2024-07-31 13:56:37 +0530

    Designed Onebox page with mailcard component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './container/Navbar';
 import Login from './pages/Login';
 import Onebox from './pages/Onebox';
 
 function App() {
-  const user = false;
   return (
     <div>
-      <Navbar />
       <Routes>
-        {
-          user ? <Route path='/' element={<Onebox />} /> : <Route path='/' element={<Login />} />
-        }
+        <Route path='/onebox' element={<Onebox />} />
+        <Route path='/' element={<Login />} />
       </Routes>
     </div>
   );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import googleLogo from "../assets/google.png"
+import Navbar from '../container/Navbar'
 
 const Login = () => {
+
+  const handleSignIn = () => {
+    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:3000/onebox"
+  }
+
   return (
-    <div className='bg-black h-screen flex justify-center items-center pb-20'>
-      <div className='py-6 px-10 bg-neutral-900 border border-gray-500 rounded-2xl text-center flex gap-4 flex-col justify-center items-center'>
-        <h3 className='text-lg text-white my-2'>
-          Create a new account
-        </h3>
-        <button className='text-gray-300 border py-3 px-28 rounded-md mb-6 flex items-center gap-2 font-extralight'>
-          <img src={googleLogo} alt='google-logo' className='' />
-          Sign Up with Google
-        </button>
-        <button className='text-white rounded bg-blue-700 py-3 px-8 mb-4'>
-          Create an Account
-        </button>
-        <p className='text-gray-500 font-light mb-4'>Already have an account? <span className='text-gray-400 hover:underline cursor-pointer'>Sign In</span></p>
+    <>
+      <Navbar />
+      <div className='bg-black h-screen flex justify-center items-center pb-20'>
+        <div className='py-6 px-10 bg-neutral-900 border border-gray-500 rounded-2xl text-center flex gap-4 flex-col justify-center items-center'>
+          <h3 className='text-lg text-white my-2'>
+            Create a new account
+          </h3>
+          <button onClick={handleSignIn} className='text-gray-300 border py-3 px-28 rounded-md mb-6 flex items-center gap-2 font-extralight'>
+            <img src={googleLogo} alt='google-logo' className='' />
+            Sign Up with Google
+          </button>
+          <button className='text-white rounded bg-blue-700 py-3 px-8 mb-4'>
+            Create an Account
+          </button>
+          <p className='text-gray-500 font-light mb-4'>Already have an account? <span className='text-gray-400 hover:underline cursor-pointer'>Sign In</span></p>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
