d924ba0fdd65df25775c1635e1e262013a318d0d
Nimish-Dureja-10 <107413787+Nimish-Dureja-10@users.noreply.github.com>
2024-08-09 14:18:26 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,7 +5,7 @@ import Navbar from '../container/Navbar'
 const Login = () => {
 
   const handleSignIn = () => {
-    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:3000/onebox"
+    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reachinbox-ai-assignment.vercel.app/onebox"
   }
 
   return (
